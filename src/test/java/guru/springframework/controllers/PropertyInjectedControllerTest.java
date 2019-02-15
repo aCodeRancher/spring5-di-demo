5409fd769539dbf8737641618f90671051acc4e1
helen <hhma@alumni.cpp.edu>
2019-02-14 23:20:58 -0500

    use MockBean and Mockito when  method

--- src/test/java/guru/springframework/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/guru/springframework/controllers/PropertyInjectedControllerTest.java
@@ -3,20 +3,28 @@ package guru.springframework.controllers;
 import guru.springframework.services.GreetingServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by jt on 5/24/17.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PropertyInjectedControllerTest {
-
+    @MockBean
     private PropertyInjectedController propertyInjectedController;
+
 
     @Before
     public void setUp() throws Exception {
-        this.propertyInjectedController = new PropertyInjectedController();
-        this.propertyInjectedController.greetingService = new GreetingServiceImpl();
+       // this.propertyInjectedController = new PropertyInjectedController();
+        //this.propertyInjectedController.greetingService = new GreetingServiceImpl();
+        when(propertyInjectedController.sayHello()).thenReturn(GreetingServiceImpl.HELLO_GURUS);
     }
 
     @Test

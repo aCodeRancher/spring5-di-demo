591886930b7b7e683ecfaf75123f813a792699a1
helen <hhma@alumni.cpp.edu>
2019-02-14 23:02:45 -0500

    use MockBean and Mockito when  method

--- src/test/java/guru/springframework/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/guru/springframework/controllers/ConstructorInjectedControllerTest.java
@@ -3,28 +3,33 @@ package guru.springframework.controllers;
 import guru.springframework.services.GreetingServiceImpl;
 import guru.springframework.services.GreetingService;
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
+import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by jt on 5/24/17.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ConstructorInjectedControllerTest {
     @MockBean
     private ConstructorInjectedController constructorInjectedController;
 
-    @MockBean
-    private GreetingService greetingService;
 
-    /*@Before
+    @Before
     public void setUp() throws Exception {
-        this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
-    }*/
+       // this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
+        when(constructorInjectedController.sayHello()).thenReturn(GreetingServiceImpl.HELLO_GURUS);
+    }
 
     @Test
     public void testGreeting() throws Exception {
+
         assertEquals(GreetingServiceImpl.HELLO_GURUS, constructorInjectedController.sayHello());
     }
 }

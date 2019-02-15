156b8842ca87a892a4bc6d178cefc5153556b733
helen <hhma@alumni.cpp.edu>
2019-02-14 22:49:22 -0500

    use MockBean instead

--- src/test/java/guru/springframework/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/guru/springframework/controllers/ConstructorInjectedControllerTest.java
@@ -1,8 +1,10 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingServiceImpl;
+import guru.springframework.services.GreetingService;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,12 +12,16 @@ import static org.junit.Assert.assertEquals;
  * Created by jt on 5/24/17.
  */
 public class ConstructorInjectedControllerTest {
+    @MockBean
     private ConstructorInjectedController constructorInjectedController;
 
-    @Before
+    @MockBean
+    private GreetingService greetingService;
+
+    /*@Before
     public void setUp() throws Exception {
         this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
-    }
+    }*/
 
     @Test
     public void testGreeting() throws Exception {

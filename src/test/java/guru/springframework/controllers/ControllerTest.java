d6f810b498ab618019aef8d2b198bc686a0bc702
helen <hhma@alumni.cpp.edu>
2019-02-16 21:27:36 -0500

    use mockito to test with primary bean

--- src/test/java/guru/springframework/controllers/ControllerTest.java
+++ src/test/java/guru/springframework/controllers/ControllerTest.java
@@ -1,29 +1,26 @@
 package guru.springframework.controllers;
 
 import org.junit.runner.RunWith;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.mockito.Mockito.when;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ControllerTest {
-    @MockBean
+
+    @Autowired
     private MyController mycontroller;
 
-    @Before
-    public void setUp() throws Exception{
-        when (mycontroller.hello()).thenReturn("Hello - Primary Greeting service");
-    }
 
     @Test
-    public void testGreeting() throws Exception{
+    public void testPrimaryGreeting() throws Exception{
         assertEquals("Hello - Primary Greeting service", mycontroller.hello());
     }
 }

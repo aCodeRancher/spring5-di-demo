a3793b60b12943046cbde460736e94d4b6eef4ec
helen <hhma@alumni.cpp.edu>
2019-02-17 17:38:14 -0500

    remove not necessary import

--- src/test/java/guru/springframework/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/guru/springframework/controllers/PropertyInjectedControllerTest.java
@@ -1,15 +1,13 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingServiceImpl;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+
 
 
 /**
